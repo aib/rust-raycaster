a31f80d7ff74ff7dccefec761f9a0a1ce2ae0d12
aib <aibok42@gmail.com>
2021-02-13 01:19:04 +0300

    Generate rays

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+mod rays;
+use rays::*;
+
+#[derive(Debug)]
+struct View {
+	look: Ray<f64>,
+	up: Vec3<f64>,
+	vfov: f64,
+	width: u32,
+	height: u32
+}
+
 fn main() {
-	println!("Hello, world!");
+	let view = View {
+		look: Ray {
+			ori: Vec3::new(0., 0., 0.),
+			dir: Vec3::new(0., 1., 0.)
+		},
+		up: Vec3::new(0., 0., 1.),
+		vfov: f64::to_radians(45.),
+		width: 10,
+		height: 10
+	};
+
+	let rays = generate_rays(view);
+
+	for ray in rays { println!("{}", ray); }
+}
+
+fn generate_rays(view:View) -> Vec<Vec3<f64>> {
+	let hfov = view.width as f64 * view.vfov / view.height as f64;
+	let right = cross(view.look.dir, view.up);
+
+	let rotate_look = |axis:Vec3<f64>, fov:f64, max:u32, val:u32, look:Vec3<f64>| {
+		let normalized = (val as f64 * 2.) / ((max - 1) as f64) - 1.;
+		rotate(axis, normalized * fov, look)
+	};
+
+	let mut rays = Vec::new();
+
+	for y in 0..view.height {
+		let y_rotated = rotate_look(right, view.vfov, view.height, y, view.look.dir);
+		for x in 0..view.width {
+			let rotated = rotate_look(-view.up, hfov, view.width, x, y_rotated);
+			rays.push(rotated);
+		}
+	}
+
+	rays
+}
+
+fn rotate (axis: Vec3<f64>, theta: f64, vec: Vec3<f64>) -> Vec3<f64> {
+	let a = (theta / 2.).cos();
+	let v = normalize(axis) * -(theta / 2.).sin();
+	let (b, c, d) = (v.x, v.y, v.z);
+	let (aa, bb, cc, dd) = (a * a, b * b, c * c, d * d);
+	let (bc, ad, ac, ab, bd, cd) = (b * c, a * d, a * c, a * b, b * d, c * d);
+
+	return Vec3 {
+		x: vec.x * (aa + bb - cc - dd) + vec.y * (2. * (bc + ad)) + vec.z * (2. * (bd - ac)),
+		y: vec.x * (2. * (bc - ad)) + vec.y * (aa + cc - bb - dd) + vec.z * (2. * (cd + ab)),
+		z: vec.x * (2. * (bd + ac)) + vec.y * (2. * (cd - ab)) + vec.z * (aa + dd - bb - cc),
+	}
+}
+
+impl std::fmt::Display for Vec3<f64> {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		write!(f, "({:+.3}, {:+.3}, {:+.3})", self.x, self.y, self.z)
+	}
 }
---
+++ src/rays.rs
@@ -0,0 +1,71 @@
+#[derive(Debug)]
+#[derive(Copy, Clone)]
+pub struct Vec3<T> {
+	pub x: T,
+	pub y: T,
+	pub z: T
+}
+
+impl<T> Vec3<T> {
+	pub fn new(x:T, y:T, z:T) -> Vec3<T> {
+		Vec3 { x: x, y: y, z: z }
+	}
+}
+
+impl <T: std::ops::Add<Output = T> + Copy> std::ops::Add<Vec3<T>> for Vec3<T> {
+	type Output = Vec3<T>;
+	fn add(self, rhs:Vec3<T>) -> Self::Output {
+		Vec3 { x: self.x + rhs.x, y: self.y + rhs.y, z: self.z + rhs.z }
+	}
+}
+
+impl <T: std::ops::Neg<Output = T>> std::ops::Neg for Vec3<T> {
+	type Output = Vec3<T>;
+	fn neg(self) -> Self::Output {
+		Vec3 { x: -self.x, y: -self.y, z: -self.z }
+	}
+}
+
+pub fn cross <T: std::ops::Mul<Output = T> + std::ops::Sub<Output = T> + Copy> (a:Vec3<T>, b:Vec3<T>) -> Vec3<T> {
+	Vec3 {
+		x: a.y * b.z - a.z * b.y,
+		y: a.z * b.x - a.x * b.z,
+		z: a.x * b.y - a.y * b.x
+	}
+}
+
+pub fn dot <T: std::ops::Mul<Output = T> + std::ops::Add<Output = T> + Copy> (a:Vec3<T>, b:Vec3<T>) -> T {
+	a.x * b.x + a.y * b.y + a.z * b.z
+}
+
+impl <T: std::ops::Mul<Output = T> + Copy> std::ops::Mul<T> for Vec3<T> {
+	type Output = Vec3<T>;
+	fn mul(self, rhs:T) -> Self::Output {
+		Vec3 { x: self.x * rhs, y: self.y * rhs, z: self.z * rhs }
+	}
+}
+
+impl <T: std::ops::Div<Output = T> + Copy> std::ops::Div<T> for Vec3<T> {
+	type Output = Vec3<T>;
+	fn div(self, rhs:T) -> Self::Output {
+		Vec3 { x: self.x / rhs, y: self.y / rhs, z: self.z / rhs }
+	}
+}
+
+pub trait Sqrt { fn sqrt(self) -> Self; }
+impl Sqrt for f64 { fn sqrt(self) -> Self { self.sqrt() } }
+
+pub fn norm <T: std::ops::Mul<Output = T> + std::ops::Add<Output = T> + Sqrt + Copy> (vec:Vec3<T>) -> T {
+	dot(vec, vec).sqrt()
+}
+
+pub fn normalize <T: std::ops::Mul<Output = T> + std::ops::Add<Output = T> + std::ops::Div<Output = T> + Sqrt + Copy> (vec:Vec3<T>) -> Vec3<T> {
+	vec / norm(vec)
+}
+
+#[derive(Debug)]
+#[derive(Copy, Clone)]
+pub struct Ray<T> {
+	pub ori: Vec3<T>,
+	pub dir: Vec3<T>
+}
